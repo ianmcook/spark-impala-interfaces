a6e9a0817df4de81c7c5528acfb03962dba55b8d
Ian Cook <icook@cloudera.com>
2018-02-09 12:35:56 -0500

    Add example of Impala SQL via ODBC

---
+++ impala-odbc.R
@@ -0,0 +1,40 @@
+# Copyright 2018 Cloudera, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script requires that the Impala ODBC driver is installed on the host
+# Modify .odbc.ini and .odbcinst.ini before running
+
+if(!"odbc" %in% rownames(installed.packages())) {
+  install.packages("odbc")
+}
+
+library(DBI)
+
+drv <- odbc::odbc()
+impala <- dbConnect(
+    drv = drv,
+    dsn = "Impala DSN"
+  )
+
+dbGetQuery(
+  impala,
+  "SELECT origin,
+    COUNT(*) AS num_departures,
+    AVG(dep_delay) AS avg_dep_delay
+  FROM flights
+  WHERE dest = 'LAS'
+  GROUP BY origin
+  ORDER BY avg_dep_delay")
+
+dbDisconnect(impala)
