a2905344a7a5c7c2bdd3d2f8eb2625ba67db2d67
Ian Cook <icook@cloudera.com>
2018-02-09 22:52:52 -0500

    Add Ibis example

---
+++ Python-examples/ibis-example.py
@@ -0,0 +1,42 @@
+# Copyright 2018 Cloudera, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Replace hdfs-hostname with an HDFS NameNode hostname before running
+
+# Replace impala-hostname with an impalad hostname before running
+
+if sys.version_info[0] == 2:
+  !pip install impyla
+  !pip install ibis-framework[impala]
+else:
+  !pip3 install impyla
+  !pip3 install ibis-framework[impala]
+
+import ibis
+ibis.options.interactive = True
+
+hdfs = ibis.hdfs_connect(host='hdfs-hostname', port=50070)
+con = ibis.impala.connect(host='impala-hostname', port=21050, hdfs_client=hdfs)
+
+flights = con.table('flights')
+
+flights \
+  .filter(flights.dest == 'LAS') \
+  .group_by('origin') \
+  .aggregate( \
+     num_departures=flights.count(), \
+     avg_dep_delay=flights.dep_delay.mean() \
+  ) \
+  .sort_by('avg_dep_delay') \
+  .execute()
